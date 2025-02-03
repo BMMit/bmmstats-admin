165f77ee1f3f5a1a9bdba3c5a12ff1470c3d3727
Humberto López Trigueros <g.lops.h1@gmail.com>
2025-02-03 21:37:53 +0100

    Image picker
    npm i required

---
+++ helpers/utils.ts
@@ -0,0 +1,12 @@
+export const generateID = () => {
+  let newDate = new Date();
+  const date = newDate
+    .toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit"
+    })
+    .replace(/[^0-9]/g, "");
+  const time = newDate.getTime().toString();
+  return date + time;
+};
